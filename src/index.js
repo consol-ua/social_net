a6dd82455136837c6598da0881a48ce6ac20409b
consol-ua <63855433+consol-ua@users.noreply.github.com>
2020-12-17 11:36:32 +0200

    main

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./redux/redux-store";
-
+// proverka
 const rootElement = document.getElementById("root");
 window.store = store;
 ReactDOM.render(
